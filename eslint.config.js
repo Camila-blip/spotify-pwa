50600870c2600b6c5090efbd3e2b9d8b2050daee
Camila Moura <camila.moura@pontua.com.br>
2025-01-02 09:42:11 -0300

    feat: migrate env to languageOptions

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,40 @@
-module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: "latest"
-    },
-    plugins: ["@typescript-eslint", "react"],
-    rules: {
-        "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
-        "react/react-in-jsx-scope": "off",
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
-        "react/prop-types": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "react/no-unknown-property": "off",
-        "react/display-name": "off",
-        "react/no-unescaped-entities": "off",
-        "@typescript-eslint/no-namespace": "off",
-        "@typescript-eslint/no-non-null-assertion": "off"
+export default [
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                browser: true,
+                es2021: true,
+                node: true
+            }
+        },
+        plugins: {
+            "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+            react: require("eslint-plugin-react")
+        },
+        rules: {
+            "react/jsx-filename-extension": [
+                1,
+                { extensions: [".ts", ".tsx"] }
+            ],
+            "react/react-in-jsx-scope": "off",
+            "linebreak-style": ["error", "unix"],
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+            "react/prop-types": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "react/no-unknown-property": "off",
+            "react/display-name": "off",
+            "react/no-unescaped-entities": "off",
+            "@typescript-eslint/no-namespace": "off",
+            "@typescript-eslint/no-non-null-assertion": "off"
+        },
+        settings: {
+            react: {
+                version: "detect"
+            }
+        }
     }
-};
+];
