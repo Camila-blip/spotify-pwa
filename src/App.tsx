211398ff26cdf42b02ce636f12255760b124e8ff
Camila Moura <camila.moura@pontua.com.br>
2025-01-02 11:35:41 -0300

    feat: sonar

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-// Função que soma dois números
-export function add(a: number, b: number): number {
-    return a + b;
-}
-
-// Função que multiplica dois números, mas não tem cobertura de testes
-export function multiply(a: number, b: number): number {
-    return a * b;
-}
-
-// Função que não segue boas práticas (complexidade alta)
-export function complexFunction(a: number, b: number, c: number): number {
-    if (a > b) {
-        if (b > c) {
-            return a + b + c;
-        } else {
-            return a - b - c;
-        }
-    } else {
-        if (a > c) {
-            return a * b * c;
-        } else {
-            return a / b / c;
-        }
-    }
-}
-
 export default function App() {
+    const teste = "teste";
     return (
         <div className="App">
             <header className="App-header">
