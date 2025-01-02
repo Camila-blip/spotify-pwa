0a2ae10d288f996b98dbdc62c47125dad642a7a0
Camila Moura <camila.moura@pontua.com.br>
2025-01-02 09:37:08 -0300

    fix: convert json to javascript eslint

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,31 +1,31 @@
-{
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:react/recommended"
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest"
     },
-    "plugins": ["@typescript-eslint", "react"],
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".ts", ".tsx"] }],
+    plugins: ["@typescript-eslint", "react"],
+    rules: {
+        "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
         "react/react-in-jsx-scope": "off",
         "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
         "react/prop-types": "off",
         "@typescript-eslint/no-explicit-any": "off",
-        "react/no-unknown-property": ["off"],
-        "react/display-name": ["off"],
-        "react/no-unescaped-entities": ["off"],
+        "react/no-unknown-property": "off",
+        "react/display-name": "off",
+        "react/no-unescaped-entities": "off",
         "@typescript-eslint/no-namespace": "off",
         "@typescript-eslint/no-non-null-assertion": "off"
     }
-}
+};
