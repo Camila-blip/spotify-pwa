ff53963ec489349d246ed6d5e7bd5e341eefa770
Camila Moura <camila.moura@pontua.com.br>
2025-01-02 11:40:33 -0300

    feat: sonar

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,32 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+// Variável não utilizada
+const unusedVariable = "This variable is not used";
+
+// Função sem cobertura de testes
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+// Função com alta complexidade
+function complexFunction(a: number, b: number, c: number): number {
+    if (a > b) {
+        if (b > c) {
+            return a + b + c;
+        } else {
+            return a - b - c;
+        }
+    } else {
+        if (a > c) {
+            return a * b * c;
+        } else {
+            return a / b / c;
+        }
+    }
+}
+
 export default function App() {
-    const teste = "teste";
     return (
         <div className="App">
             <header className="App-header">
